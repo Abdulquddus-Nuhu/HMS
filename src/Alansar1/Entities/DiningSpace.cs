acee6c051d47fe5e75bb7f6a242ab2cebf39d5d4
Abdulquddus Nuhu <abdulquddusnuhu@gmail.com>
2024-09-16 18:15:35 +0100

    removed redundant files

--- src/Alansar1/Entities/DiningSpace.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Alansar.Entities
-{
-    public class DiningSpace : BaseEntity
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Capacity { get; set; }
-    }
-}
