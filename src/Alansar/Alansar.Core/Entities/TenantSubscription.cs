85d49f2eba66b1502ec0dca4e94d81702373d3ef
Abdulquddus Nuhu <abdulquddusnuhu@gmail.com>
2024-09-19 23:45:26 +0100

    monthly and yearly plan ui

---
+++ src/Alansar/Alansar.Core/Entities/TenantSubscription.cs
@@ -0,0 +1,24 @@
+﻿using Alansar.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alansar.Core.Entities
+{
+    public class TenantSubscription : BaseEntity
+    {
+        public int TenantId { get; set; }
+        public Tenant? Tenant { get; set; }
+
+        public SubscriptionPlanType PlanType { get; set; }
+        public BillingCycle BillingCycle { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool HasPaid { get; set; } = false;
+        public decimal Amount { get; set; }
+        public decimal FeeAmount { get; set; }
+    }
+}
---
+++ src/Alansar/Alansar/Controllers/TenantController.cs
@@ -0,0 +1,53 @@
+﻿using Alansar.Core.Entities;
+using Alansar.Core.Entities.Identity;
+using Alansar.Core.Models.Requests;
+using Alansar.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alansar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TenantController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public TenantController(AppDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateTenant([FromBody] CreateTenantRequest request)
+        {
+            var tenant = new Tenant()
+            {
+                SchoolName = request.SchoolName,
+                Email = request.Email,
+            };
+
+            var tenantSubscription = new TenantSubscription()
+            {
+                TenantId = tenant.Id,
+                PlanType = request.PlanType,
+                BillingCycle = request.BillingCycle,
+                HasPaid = false,
+            };
+
+            if (request.PlanType == Core.Enums.SubscriptionPlanType.Free)
+            {
+                tenantSubscription.StartDate = DateTime.UtcNow;
+                //todo: change later
+                tenantSubscription.EndDate = DateTime.UtcNow.AddMinutes(5);
+            }
+
+            //_context.Students.Add(newStudent);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
